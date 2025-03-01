aca31048417340d644b8a309be2eb62c73f1fcd8
feng <succeed_happu@outlook.com>
2025-03-01 15:14:31 +0800

    fix(vision): 修复 vision 检测数量计算错误
    - 更新版本号至 Sengo2 v1.0.1
    - 添加 v_start_id 变量以记录起始 ID
    - 修正检测数量计算公式：使用 stop_id - v_start_id + 1 替代原先的 stop_id - start_id + 1
    - 优化代码结构，提高可读性和准确性

--- src/Sengo2.py
+++ src/Sengo2.py
@@ -1,4 +1,4 @@
-__version__ = "Sengo2 v1.0.0"
+__version__ = "Sengo2 v1.0.1"
 __license__ = "http://unlicense.org"
 
 import ustruct  # pylint: disable=import-error
@@ -599,7 +599,7 @@ class SentryUartMethod:
 
         try_time = 0
         vision_state.detect = 0
-
+        v_start_id = -1
         while True:
             err, data = self.__protocol_read()
             #print("read",hex(err), hex(data[0]))
@@ -609,6 +609,8 @@ class SentryUartMethod:
                         vision_state.frame = data[2]
                         start_id = data[4]
                         stop_id = data[5]
+                        if v_start_id == -1:
+                            v_start_id = start_id
 
                         if SENTRY_MAX_RESULT < stop_id:
                             return (SENTRY_UNSUPPORT_PARAM, vision_state)
@@ -619,9 +621,9 @@ class SentryUartMethod:
                         if sengo2_vision_e.kVisionQrCode == vision_type:
                             vision_state.detect = 1
                         else:
-                            vision_state.detect = stop_id-start_id+1
+                            vision_state.detect = stop_id-v_start_id+1
 
-                        for i in range(vision_state.detect):
+                        for i in range(stop_id-start_id+1):
                             v_id = i+start_id-1
                             vision_state.result[v_id].data1 = data[10 *
                                                                    i + 6] << 8 | data[10 * i + 7]
